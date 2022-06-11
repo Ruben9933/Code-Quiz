cd93cc3070c8c03b963dac845989a36898d1fe2a
Ruben9933 <105179308+Ruben9933@users.noreply.github.com>
2022-06-11 14:41:00 -0400

    css added/html done/js var added

---
+++ script.js
@@ -0,0 +1,21 @@
+var questions = [{
+    question: "1. What is 2+2?",
+    choices: ["5", "11", "4", "turtle"],
+    correctAnswer: 2
+}, {
+    question: "2. What color is the sky?",
+    choices: ["Pink", "Blue", "Yellow", "Chicken"],
+    correctAnswer: 1
+}, {
+    question: "3. What sound does a cow make?",
+    choices: ["Moo", "Cluck", "Roar", "Vroom"],
+    correctAnswer: 0
+}, {
+    question: "4. How many wheels does a tricycle have?",
+    choices: ["3", "4", "2", "6"],
+    correctAnswer: 0
+}, {
+    question: "5. ",
+    choices: ["s", "", "", ""],
+    correctAnswer: 0
+}];
